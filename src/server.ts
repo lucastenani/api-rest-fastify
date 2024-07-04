cf592e555212631da5aa43811b53b282c7eee350
lucastenani <lucas.tenani3@gmail.com>
2024-07-04 01:04:42 -0300

    Treating environment variables with Zod

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import crypto from 'node:crypto'
 import fastify from 'fastify'
 
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -22,7 +23,7 @@ app.get('/hello', async () => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running')
